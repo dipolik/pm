cca0dcb4eae41821bd0d32ef14bd7568d735649d
dipolik <dipolik4@gmail.com>
2022-12-26 16:03:39 +0200

    Half block image changed

--- page-homepage.php
+++ page-homepage.php
@@ -128,7 +128,13 @@ get_header();
 
 	<!-- Half block -->
 	<section class="section s-half-block s-half-block--content-left">
-        <div class="bg-img"></div>
+        <div class="bg-img">
+        <img class="lazy washmachine-img" 
+            src="<?php bloginfo('template_url'); ?>/img/loading.png" 
+            data-src="<?php bloginfo('template_url'); ?>/img/washmachine-mob.png" 
+            data-srcset="<?php bloginfo('template_url'); ?>/img/washmachine-mob.png" 
+            alt="Комфортний ремонт пральних машин">
+        </div>
         <div class="bg-color"></div>
         <div class="container">
             <div class="row">
