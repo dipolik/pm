2238193be547b9bdd681bf60a910a84520334fee
dipolik <dipolik4@gmail.com>
2022-12-28 22:21:29 +0200

    gtag on scroll

--- header.php
+++ header.php
@@ -27,14 +27,34 @@
     <meta name="msapplication-TileColor" content="#00aba9">
     <meta name="theme-color" content="#ffffff">
 
+
 	<!-- Google Tag Manager -->
+	<script>
+		var fired = false;
+
+		window.addEventListener('scroll', () => {
+		    if (fired === false) {
+		        fired = true;
+		        setTimeout(() => {
+					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-KHRB8S8');
+		        }, 1000)
+			}
+		});
+	</script>
+	<!-- End Google Tag Manager -->
+
+
+<!-- 	
 	<script rel="dns-prefetch">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-KHRB8S8');</script>
-	<!-- End Google Tag Manager -->
-
+	})(window,document,'script','dataLayer','GTM-KHRB8S8');</script> -->
+	
 
 	<?php wp_head(); ?>
 </head>
